8c0b6f9fc72bd05a2cfcc320f6992ea348e3dd01
Harrison Bacordo <harrisonbacordo@gmail.com>
2018-07-23 16:08:04 +1200

    Jumbotron progress, still needs tweaking

---
+++ src/components/blogs/blogs.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import BlogPost from '../blog-post/blog-post';
+
+class Blogs extends Component {
+    render() {
+        return (
+            <BlogPost postTitle="Test Title">This is a test</BlogPost>
+        )
+    }
+}
+
+export default Blogs;
--- src/components/jumbotron/jumbotron.js
+++ src/components/jumbotron/jumbotron.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
+import bg from '../../res/img/headerBackground1.jpg';
 
 class Jumbotron extends Component {
     render() {
-        const {backgroundImage, headerText, subHeaderText} = this.props;
-        const {containerStyle} = styles;
-        <div style={containerStyle}>
-            <h1>{headerText}</h1>
-            <p>{subHeaderText}</p>
-        </div>;
-
         const styles = {
             containerStyle: {
-                backgroundImage: {backgroundImage}
+                backgroundImage: "url(" + bg + ")",
+                backgroundSize: 'cover',
+                textAlign: 'Center',
+                padding: '15%',
+                color: 'white'
+            },
+            headerStyle: {
+                fontSize: '300%'
+            },
+            subHeaderStyle: {
+                fontSize: '150%'
             }
         }
+        const { backgroundImage, headerText, subHeaderText } = this.props;
+        const { containerStyle, headerStyle, subHeaderStyle } = styles;
+        return (
+            <div style={containerStyle}>
+                <h1 style={headerStyle}>{headerText}</h1>
+                <p style={subHeaderStyle}>{subHeaderText}</p>
+            </div>
+        );
     }
 }
 
